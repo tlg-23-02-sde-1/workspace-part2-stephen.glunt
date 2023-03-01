2837a95f6ded7444527a7868546294770861aa8d
Stephen Glunt <stephen.glunt@tlgcohort.com>
2023-03-01 13:24:07 -0800

    Added lifecycle test checks to CalculatorTest.java.

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -10,14 +10,37 @@ package com.math;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class CalculatorTest {
+
+    // object(s) under test - called a "fixture" in JUnit
+    private Calculator calc;
+
+    @BeforeClass
+    public static void initializeEntireTestRun() {
+        System.out.println("initializeEntireTestRun");
+    }
+
+    @AfterClass
+    public static void finalizeTestRun() {
+        System.out.println("finalizeTestRun");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
 
     @Test
     public void testAdd() {
         System.out.println("Test Add");
-        Calculator calc = new Calculator();
         assertEquals(5, calc.add(1, 4));  // expected, actual
         assertEquals(12, calc.add(1, 5, 8, -2));
     }
@@ -25,14 +48,12 @@ public class CalculatorTest {
     @Test
     public void testDivide() {
         System.out.println("Test Divide");
-        Calculator calc = new Calculator();
         assertEquals(String.valueOf(3.5), 3.5, calc.divide(7, 2), 0.001);
     }
 
     @Test
     public void testIsEven(){
         System.out.println("Test isEven");
-        Calculator calc = new Calculator();
         assertTrue(calc.isEven(13982));
         assertFalse(calc.isEven(1953));
     }
@@ -40,7 +61,6 @@ public class CalculatorTest {
     @Test
     public void testSubtract() {
         System.out.println("Test subtract");
-        Calculator calc = new Calculator();
         assertEquals(-5, calc.subtract(10, 5, 5,5));
     }
 }
