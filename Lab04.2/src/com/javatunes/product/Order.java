37ef7352cd01c29e79337b1261c82575c1045eba
Stephen Glunt <stephen.glunt@tlgcohort.com>
2023-03-02 14:00:35 -0800

    Initial work on Lab04.2, will find out if there is more to do.

--- Lab04.2/src/com/javatunes/product/Order.java
+++ Lab04.2/src/com/javatunes/product/Order.java
@@ -11,22 +11,33 @@ package com.javatunes.product;
 import java.util.Collection;
 
 public class Order {
-  private String id;
-  
-  public Order(String id) {
-    this.id = id;
-  }
-  
-  /**
-   * TODO:
-   * get the items from the cart and iterate over them, print each item's product code
-   * get cart total and print
-   */
-  public void processCart(Object reference_to_cart) {
-    
-  }
-  
-  public String getId() {
-    return id;
-  }
+    private String id;
+
+    public Order(String id) {
+        this.id = id;
+    }
+
+    /**
+     * TODO:
+     * get the items from the cart and iterate over them, print each item's product code
+     * get cart total and print
+     */
+    public void processCart(Object reference_to_cart) {
+        ShoppingCart<Product> cart = null;
+        if(reference_to_cart instanceof ShoppingCart){
+            cart = (ShoppingCart<Product>) reference_to_cart;
+        } else {
+            return;
+        }
+
+        System.out.printf("Order: %s contains the following:\n", this.getId());
+        for(var item : cart.allItems()){
+            System.out.println(item.getCode());
+        }
+        System.out.printf("Order Total: $%.2f", cart.total());
+    }
+
+    public String getId() {
+        return id;
+    }
 }
--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,15 +12,34 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
-  // storage for the cart's contents
-  private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
-  
-  public void addItem(T item) {
-    items.add(item);
-  }
-  
-  public void removeItem(T item) {
-    items.remove(item);
-  }
+public class ShoppingCart<T extends Product> {
+    // storage for the cart's contents
+    private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
+
+    /**
+     * Returns unmodifiable view of the collection
+     */
+    public Collection<T> allItems(){
+        return Collections.unmodifiableCollection(items);
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public double total(){
+        double total = 0.;
+        for(var item : items){
+            total += item.getPrice();
+        }
+        return total;
+    }
 }
