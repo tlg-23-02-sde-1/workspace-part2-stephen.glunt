7c837bf1dd320ad330febd45f9c8768c6104a64e
Stephen Glunt <stephen.glunt@tlgcohort.com>
2023-03-03 08:05:31 -0800

    Completed all tasks for this lab.

--- Lab04.1/test/com/entertainment/catalog/CatalogTest.java
+++ Lab04.1/test/com/entertainment/catalog/CatalogTest.java
@@ -9,19 +9,85 @@
 package com.entertainment.catalog;
 
 import static org.junit.Assert.*;
+
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
 import com.entertainment.Television;
 
 public class CatalogTest {
-  
-  /**
-   * Contract: a no-matches result should be an empty collection (not null).
-   */
-  @Test
-  public void testFindByBrandNoMatches() {
-    Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
-    assertNotNull(tvs);
-    assertTrue(tvs.isEmpty());
-  }
+
+    /**
+     * Contract: a no-matches result should be an empty collection (not null).
+     */
+    @Test
+    public void findByBrand_shouldReturnEmptyCollection_whenBrandNotFound() {
+        Collection<Television> tvs = Catalog.findByBrand("NO-MATCHES");
+        assertTrue(tvs.isEmpty());
+    }
+
+    @Test
+    public void findByBrand_shouldReturnCollection_withAllMatchingBrands() {
+        String brand = "Sony";
+        Collection<Television> tvs = Catalog.findByBrand(brand);
+        assertEquals(7, tvs.size());
+        for(var item : tvs){
+            assertEquals(brand, item.getBrand());
+        }
+    }
+
+    @Test
+    public void findByBrands_shouldReturnEmptyMap_whenNoBrandsPassed(){
+        Map<String, Collection<Television>> tvs = Catalog.findByBrands();
+        assertTrue(tvs.isEmpty());
+    }
+
+    @Test
+    public void findByBrands_shouldReturnMap_WithEntryForEachBrandSupplied() {
+        String[] brands = {"Sony", "RCA", "Zenith", "D", "E"};
+        Map<String, Collection<Television>> tvs = Catalog.findByBrands(brands);
+        assertEquals(brands.length, tvs.size());
+        for(var brand : brands) {
+            assertTrue(tvs.containsKey(brand));
+        }
+        for(var brand : brands) {
+            Collection<Television> sonyTvs = tvs.get(brand);
+            int size = Catalog.findByBrand(brand).size();
+            assertEquals(size, sonyTvs.size());
+            for(var tv : sonyTvs){
+                assertEquals(brand, tv.getBrand());
+            }
+        }
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void getInventory_shouldBreak_whenTryingToEditInventory(){
+        Collection<Television> myCat = Catalog.getInventory();
+        myCat.clear();
+    }
+
+    @Test
+    public void removeDuplicates_shouldRemoveAllDuplicates(){
+        Set<Television> tvs = new HashSet<>(Catalog.getInventory());
+        Television previous = null;
+        for(var tv : tvs){
+            assertFalse(tv.equals(previous));
+        }
+    }
+
+    @Test
+    public void findLoudest_shouldFindLoudest_inInventory(){
+        int loudestVolume = 0;
+        Television loudest = null;
+        for(var tv : Catalog.getInventory()){
+            if(tv.getVolume() > loudestVolume){
+                loudestVolume = tv.getVolume();
+                loudest = tv;
+            }
+        }
+        assertEquals(loudestVolume, loudest.getVolume());
+    }
 }
