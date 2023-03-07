b11d90968808dc11f04ce173342982358448d847
Stephen Glunt <stephen.glunt@tlgcohort.com>
2023-03-07 09:04:40 -0800

    DummyEmployee and anonymous Employee subclass for equals testing.

--- Lab03.2/Test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/Test/com/javatunes/personnel/EmployeeTest.java
@@ -10,18 +10,30 @@ import static org.junit.Assert.*;
 public class EmployeeTest {
     private Employee emp1;
     private Employee emp2;
+    private Employee emp3;
+    private Employee emp4;
     @Before
     public void setUp() throws Exception {
-        emp1 = new SalariedEmployee("Joseph", Date.valueOf("2010-10-10"));
-        emp2 = new SalariedEmployee("Joseph", Date.valueOf("2010-10-10"));
+//        emp1 = new SalariedEmployee("Joseph", Date.valueOf("2010-10-10"));
+//        emp2 = new SalariedEmployee("Joseph", Date.valueOf("2010-10-10"));
+        emp1 = getDummyEmployee("Joseph", Date.valueOf("2010-10-10"));
+        emp2 = getDummyEmployee("Joseph", Date.valueOf("2010-10-10"));
+        emp3 = new DummyEmployee("Joseph", Date.valueOf("2010-10-10"));
+        emp4 = new DummyEmployee("Joseph", Date.valueOf("2010-10-10"));
     }
 
+    private Employee getDummyEmployee(String name, Date date){
+        return new Employee() {
+            public double pay() {return 0;}
+            public double payTaxes() {return 0;}
+        };
+    }
 
 
     @Test
     public void equals_shouldReturnFalse_differentName_sameHireDate(){
-        emp2.setName("Jack");
-        assertNotEquals(emp1, emp2);
+        emp3.setName("Jack");
+        assertNotEquals(emp4, emp3);
     }
 
     @Test
@@ -33,5 +45,22 @@ public class EmployeeTest {
     @Test
     public void equals_shouldReturnTrue_allPropertiesSame() {
         assertEquals(emp1, emp2);
+    }
+
+    // NAMED MEMBER-LEVEL INNER CLASSES
+    private class DummyEmployee extends Employee {
+        DummyEmployee (String name, Date date){
+            super(name, date);
+        }
+
+        @Override
+        public double pay() {
+            return 0;
+        }
+
+        @Override
+        public double payTaxes() {
+            return 0;
+        }
     }
 }
