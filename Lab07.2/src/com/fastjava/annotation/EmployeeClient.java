16a452cb57bcff4979f61e120c01aeaa5aa0f590
Stephen Glunt <stephen.glunt@tlgcohort.com>
2023-03-08 08:16:12 -0800

    Worked on the @Fast annotation.

---
+++ Lab07.2/src/com/fastjava/annotation/EmployeeClient.java
@@ -0,0 +1,11 @@
+package com.fastjava.annotation;
+
+import com.javatunes.personnel.Employee;
+
+
+class EmployeeClient {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        emp1.work();
+    }
+}
